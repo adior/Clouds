bcfb9d9f2b3c0749fe94704be5607edc7aea174d
Tcc <1178608176@qq.com>
2019-05-04 12:26:08 +0800

    commit

--- app/src/main/java/example/com/widget/CoordinatorActivity.java
+++ app/src/main/java/example/com/widget/CoordinatorActivity.java
@@ -1,25 +1,20 @@
-package example.com.cloud;
+package example.com.widget;
 
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import example.com.cloud.R;
 
 
 public class CoordinatorActivity extends AppCompatActivity {
